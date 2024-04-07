ae73924abb678afa14c4088f21e1e77ec4ab5aee
chengzi <2351850385@qq.com>
2024-04-07 15:36:40 +0800

    222

--- Node_JavaWeb/src/main/webapp/2-文件操作/4-path模块应用.js
+++ Node_JavaWeb/src/main/webapp/2-文件操作/4-path模块应用.js
@@ -1,7 +1,7 @@
 let fs = require("fs");
 let path = require("path");
 
-console.log(__dirname,'111');
+console.log(__dirname,'222');
 console.log(__dirname+"\\name.txt");
 console.log(path.join(__dirname,"name.txt"));
 console.log(path.resolve(__dirname, "name.txt"));
